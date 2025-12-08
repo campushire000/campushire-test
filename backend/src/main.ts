2881a8293c4000d3b87d4b5ee902b9561842b39f
murali1511 <72644159+murali1511@users.noreply.github.com>
2025-12-08 15:11:47 +0530

    environment, authentication with Roles added

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.enableCors(); // This allows Angular (4200) to call Nest (3000)
   await app.listen(3000);
 }
